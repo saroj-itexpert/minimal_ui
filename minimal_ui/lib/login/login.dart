805490f6842ab370a5df625fcdb5186f565a35aa
unknown <maharjan.saroj93@gmail.com>
2019-01-02 12:46:03 +0545

    final login ui

--- minimal_ui/lib/login/login.dart
+++ minimal_ui/lib/login/login.dart
@@ -6,8 +6,29 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final TextEditingController _userController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _userController = new TextEditingController();
+  final TextEditingController _passwordController =new TextEditingController();
+  String _welcomeString = "";
+
+
+  void _erase(){
+    setState(() {
+          _userController.clear();
+          _passwordController.clear();
+        });
+  }
+
+  void _showWelcome(){
+    setState(() {
+          if(_userController.text.isNotEmpty 
+              && _passwordController.text.isNotEmpty
+          ){
+            _welcomeString = _userController.text;
+          }else{
+            _welcomeString = "Enter Values";
+          }
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,93 +39,94 @@ class _LoginState extends State<Login> {
         backgroundColor: Colors.blueAccent,
       ),
       backgroundColor: Colors.blueGrey,
-      body: Container(
-        alignment: Alignment.topCenter,
-        child: Column(
-          children: <Widget>[
-            Image.asset(
-              'assets/images/face.png',
-              width: 80.0,
-              height: 80.0,
-              color: Colors.lightGreen,
-            ),
-            //form starts
-            SizedBox(height: 30.0,),
+      body: ListView(
+        children: <Widget>[
+
+       
             Container(
-              width: 400.0,
-              height: 300.0,
-              color: Colors.white,
-              child: Column(
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: <Widget>[
+              Image.asset(
+                'assets/images/face.png',
+                width: 80.0,
+                height: 80.0,
+                color: Colors.lightGreen,
+              ),
+              //form starts
+              SizedBox(height: 30.0,),
+              Container(
+                width: 400.0,
+                height: 300.0,
+                color: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: _userController,
+                      cursorColor: Colors.red,
+                      decoration: InputDecoration(
+                        hintText: "Enter Your Username",
+                        labelText: "Username",
+                        icon: Icon(Icons.person),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextField(
+                      controller: _passwordController,
+                      cursorColor: Colors.red,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter Your Password",
+                        labelText: "Password",
+                        icon: Icon(Icons.lock),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        RaisedButton(
+                          color: Colors.greenAccent,
+                          child: Text("Login"),
+                          onPressed: _showWelcome,
+                        ),
+                        RaisedButton(
+                          child: Text("Sign Up"),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    FlatButton(
+                      child: Text("RESET"),
+                      onPressed: _erase,
+                    ),
+                  ],
+                ),
+              ),
+              //FORM ENDS
+              SizedBox(height: 20.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  TextField(
-                    controller: _userController,
-                    cursorColor: Colors.red,
-                    decoration: InputDecoration(
-                      hintText: "Enter Your Username",
-                      labelText: "Username",
-                      icon: Icon(Icons.person),
+                  Text(
+                    "Welcome, $_welcomeString",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontFamily: 'Oxygen',
+                      fontWeight: FontWeight.w500
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  TextField(
-                    controller: _passwordController,
-                    cursorColor: Colors.red,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Enter Your Password",
-                      labelText: "Password",
-                      icon: Icon(Icons.lock),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      RaisedButton(
-                        color: Colors.greenAccent,
-                        child: Text("Login"),
-                        onPressed: () {},
-                      ),
-                      RaisedButton(
-                        child: Text("Sign Up"),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                  FlatButton(
-                    child: Text("RESET"),
-                    onPressed: () {
-                      setState(() {
-                        _userController.clear();
-                        _passwordController.clear();
-                      });
-                    },
                   )
                 ],
-              ),
-            ),
-            //FORM ENDS
-            SizedBox(height: 20.0,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Welcome, Saroj",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30.0,
-                    fontFamily: 'Oxygen',
-                    fontWeight: FontWeight.w500
-                  ),
-                )
-              ],
-            )
-          ],
+              )
+            ],
+          ),
         ),
+         ],
       ),
     );
   }
