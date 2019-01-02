b4457d05f9b658305bb9707edf9aafdd9eafc044
unknown <maharjan.saroj93@gmail.com>
2019-01-02 10:54:46 +0545

    sm-pauloLogin

--- minimal_ui/lib/main.dart
+++ minimal_ui/lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import './ui/rain.dart';
+import './login/login.dart';
 
 void main() {
-  var title = "Make It Rain";
+  var title = "Login UI";
 
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: title,
-      home: Rain(),
+      home: Login(),
     ),
   );
 }
